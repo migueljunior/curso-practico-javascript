f93b51d59c25ed91dfe989f18c10626c05a004dd
Junior Rodriguez <miguel.ajr89@gmail.com>
2022-08-02 20:41:26 -0400

    Added example of addEventListener

--- src/scriptCalculo.js
+++ src/scriptCalculo.js
@@ -3,10 +3,17 @@ const input1 = document.querySelector('#calculo1');
 const input2 = document.getElementById('calculo2');
 const btn = document.getElementById('btnCalcular');
 const result = document.getElementById('result');
+const form = document.getElementById('form');
 
-function btnOnClick(){
+btn.addEventListener('click', btnOnClick);
+
+function btnOnClick(event){
+    console.log({event});
+    event.preventDefault();
     let resultado = parseInt(input1.value) + parseInt(input2.value);
     let resultadoFinal = 'Resultado de la suma es ' + resultado;
     console.log(resultadoFinal);
     result.innerText = resultadoFinal;
 };
+
+form.addEventListener('submit', btnOnClick);
